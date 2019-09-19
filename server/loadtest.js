abcca21235b819a0adcb52bd3d11dda87f969fec
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-19 12:31:28 -0500

    Loads random item that is in the last ten percent of the database

--- server/loadtest.js
+++ server/loadtest.js
@@ -1,23 +1,26 @@
 const loadtest = require('loadtest');
 
 function statusCallback(error, result, latency) {
-  console.log('Current latency %j', latency);
-  console.log('----');
-  console.log('Request elapsed milliseconds: ', result.requestElapsed);
+  // console.log('Current latency %j', latency);
+  // console.log('----');
+  // console.log('Request elapsed milliseconds: ', result.requestElapsed);
   console.log('Request index: ', result.requestIndex);
 }
 
 const options = {
   url: 'http://localhost:4444',
-  maxRequests: 1000000,
+  maxRequests: 500000,
   statusCallback: statusCallback,
-  rps: 10000
+  requestsPerSecond: 1000,
+  //concurrency: 10
+
 };
 
 loadtest.loadTest(options, function(error, result) {
   if (error) {
     return console.error('Got an error: %s', error);
   }
+  console.log(result)
   console.log('Tests run successfully');
 });
 
